e8217740919bcda8d98b3d76fb2cdbe129ac349a
peiwen.lv <peiwen.lv@feiniu.com>
2024-06-04 14:36:02 +0800

    冒泡排序排序

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,8 +1,5 @@
-
-
 package sort;
 
-import java.util.Arrays;
 
 /**
  * @Description 冒泡排序
@@ -12,26 +9,67 @@ import java.util.Arrays;
  **/
 public class BubbleSort {
 
-    public static void main(String[] args) {
-//        int[] a = new int[]{5, 16, 9, 1, 5, 32, 4, 87, 31, 5, 2};
-        int[] a = new int[]{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
-        process(a);
-        System.out.println(Arrays.toString(a));
-    }
 
-
-    private static void process(int[] arr) {
-        if (arr == null || arr.length == 1) {
+    /**
+     * 冒泡排序的基本思想是通过遍历要排序的列表，比较每对相邻项，然后交换顺序错误的项，每一轮遍历都会将未排序部分的最大
+     * （或最小）元素“冒泡”到序列的末尾。
+     *
+     * 算法步骤：
+     * 1.比较相邻的元素。如果第一个比第二个大（小），就交换他们。
+     * 2.对每一对相邻元素做同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大（或最小）的数。
+     * 针对所有的元素重复以上的步骤，除了最后已经排序的元素。
+     * 重复步骤1～3，直到排序完成。
+     *
+     * 时间复杂度：
+     * 最好情况O(n):如果数组已经是排序好的。
+     * 最坏和平均情况(O(n^2))。
+     *
+     * 空间复杂度：
+     * (O(1))，因为它是一个原地排序算法。
+     *
+     */
+    private static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 0; i <= arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    arr[j] = arr[j] ^ arr[j + 1];
-                    arr[j + 1] = arr[j] ^ arr[j + 1];
-                    arr[j] = arr[j] ^ arr[j + 1];
+                    swap(arr, j, j + 1);
                 }
             }
         }
     }
+
+    private static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = CompareData.generateRandomArray(maxSize, maxValue);
+            int[] arr2 = CompareData.copyArray(arr1);
+            bubbleSort(arr1);
+            CompareData.comparator(arr2);
+            if (!CompareData.isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = CompareData.generateRandomArray(maxSize, maxValue);
+        CompareData.printArray(arr);
+        bubbleSort(arr);
+        CompareData.printArray(arr);
+    }
+
+
 }
---
+++ src/sort/CompareData.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 1997-2020 康成投资（中国）有限公司
+ *
+ * http://www.rt-mart.com
+ *
+ * 版权归本公司所有，不得私自使用、拷贝、修改、删除，否则视为侵权
+ */
+
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * @Description 对数器
+ *
+ * @Program algorithm
+ * @Author peiwen.lv
+ * @Date 2024-06-04
+ **/
+public class CompareData {
+
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        System.arraycopy(arr, 0, res, 0, arr.length);
+        return res;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int anArr : arr) {
+            System.out.print(anArr + " ");
+        }
+        System.out.println();
+    }
+}
