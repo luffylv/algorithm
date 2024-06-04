0f34a94f7a583e8b4f09b3a70852ca4616fc29a2
peiwen.lv <peiwen.lv@feiniu.com>
2024-06-04 13:42:57 +0800

    选择排序

--- src/sort/SelectSort.java
+++ src/sort/SelectSort.java
@@ -12,28 +12,114 @@ import java.util.Arrays;
  **/
 public class SelectSort {
 
-
-    public static void main(String[] args) {
-        int[] a = new int[]{5, 16, 9, 1, 5, 32, 4, 87, 31, 5, 2};
-        process(a);
-        System.out.println(Arrays.toString(a));
-    }
-
-
-    private static void process(int[] arr) {
-        if (arr == null || arr.length == 1) {
+    /**
+     * 选择排序的基本思想是在每一轮选择最小（或最大）的元素，然后将其放置在数组的起始位置，这个过程重复进行，
+     * 每次迭代都从剩余未排序的元素中选择最小（或最大）的元素，放到已排序序列的末尾
+     *
+     * 算法步骤：
+     * 1.从未排序的序列中找到最小（或最大）元素。
+     * 2.将找到的最小（或最大）元素和未排序序列的第一个元素交换。
+     * 3.重复以上步骤，每次迭代都减少未排序序列的长度。
+     *
+     */
+    private static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            // i ～ N-1上找最小值的下标
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    arr[i] = arr[i] ^ arr[j];
-                    arr[j] = arr[i] ^ arr[j];
-                    arr[i] = arr[i] ^ arr[j];
-                }
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
             }
+            swap(arr, i, minIndex);
         }
     }
 
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // for test
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int arr[] = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    private static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        System.arraycopy(arr, 0, res, 0, arr.length);
+        return res;
+    }
+
+    // for test
+    private static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int anArr : arr) {
+            System.out.print(anArr + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            selectionSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        selectionSort(arr);
+        printArray(arr);
+    }
 
 }
